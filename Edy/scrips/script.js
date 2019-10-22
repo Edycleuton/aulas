93b1852eb82455e49655215cb35bd5c8cffcf025
Wellingtom Ramos <wellingtom.h@hotmail.com>
2019-10-22 11:36:07 -0300

    Ja disse para fazer commit

--- Edy/scrips/script.js
+++ Edy/scrips/script.js
@@ -1,5 +1,11 @@
 const $header = document.querySelector('header');
 window.addEventListener('scroll',toggleHeader,false);
 function toggleHeader(){
-    console.log('Rolagem de pagYoff');
+   if (window.pageXOffset > 0 && $header.classList.contains('max-header')) {
+    $header.classList.remove('max-header');
+    $header .classList.add('min-header'); 
+   }  else if(window.pageYOffset == 0 &&  $header.classList.contains('min-header')) {
+       $header.classList.add('max-header');
+    $header.classList.remove('min-header');
+   }
 }
